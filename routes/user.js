0dbf4b5d302d8b7a9da41d045de1f116a30a62f4
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-11-26 19:40:37 +0530

    cart Schema & Modify signup ROute

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,15 @@
 var router = require('express').Router();
-var User = require('../models/user');
 var passport = require('passport');
+var async = require('async');
+
+var User = require('../models/user');
+var Cart = require('../models/cart');
 var passportConf = require('../config/passport');
 
 
-router.get('/login', function(req, res) {
+router.get('/login', function (req, res) {
   if (req.user) return res.redirect('/');
-  res.render('accounts/login', { message: req.flash('loginMessage')});
+  res.render('accounts/login', { message: req.flash('loginMessage') });
 });
 
 router.post('/login', passport.authenticate('local-login', {
@@ -15,8 +18,8 @@ router.post('/login', passport.authenticate('local-login', {
   failureFlash: true
 }));
 
-router.get('/profile', function(req, res, next) {
-  User.findOne({ _id: req.user._id }, function(err, user) {
+router.get('/profile', function (req, res, next) {
+  User.findOne({ _id: req.user._id }, function (err, user) {
     if (err) return next(err);
 
     res.render('accounts/profile', { user: user });
@@ -26,58 +29,69 @@ router.get('/profile', function(req, res, next) {
 
 });
 
-router.get('/signup', function(req, res, next) {
+router.get('/signup', function (req, res, next) {
   res.render('accounts/signup', {
     errors: req.flash('errors')
   });
 });
 
-router.post('/signup', function(req, res, next) {
-  var user = new User();
+router.post('/signup', function (req, res, next) {
 
-  user.profile.name = req.body.name;
-  user.email = req.body.email;
-  user.password = req.body.password;
-  user.profile.picture = faker.image.avatar();
+  async.waterfall([
+    function (callback) {
+      var user = new User();
 
-  User.findOne({ email: req.body.email }, function(err, existingUser) {
+      user.profile.name = req.body.name;
+      user.email = req.body.email;
+      user.password = req.body.password;
+      user.profile.picture = user.gravatar();
 
-    if (existingUser) {
-      req.flash('errors', 'Account with that email address already exists');
-      return res.redirect('/signup');
-    } else {
-      user.save(function(err, user) {
-        if (err) return next(err);
+      User.findOne({ email: req.body.email }, function (err, existingUser) {
 
-        req.logIn(user, function(err) {
+        if (existingUser) {
+          req.flash('errors', 'Account with that email address already exists');
+          return res.redirect('/signup');
+        } else {
+          user.save(function (err, user) {
+            if (err) return next(err);
+            callback(null, user)
+          });
+        }
+      });
+    },
+    function (user) {
+      var cart = new Cart();
+      cart.owner = user._id;
+      cart.save(function(err){
+        if(err) return next(err);
+        req.logIn(user, function (err) {
           if (err) return next(err);
           res.redirect('/profile');
-
-        })
+        });
       });
     }
-  });
+  ]);
 });
 
 
-router.get('/logout', function(req, res, next) {
+router.get('/logout', function (req, res, next) {
   req.logout();
   res.redirect('/');
 });
 
-router.get('/edit-profile', function(req, res, next) {
-  res.render('accounts/edit-profile', { message: req.flash('success')});
+router.get('/edit-profile', function (req, res, next) {
+  res.render('accounts/edit-profile', { message: req.flash('success') });
 });
 
-router.post('/edit-profile', function(req, res, next) {
-  User.findOne({ _id: req.user._id }, function(err, user) {
+router.post('/edit-profile', function (req, res, next) {
+  User.findOne({ _id: req.user._id }, function (err, user) {
 
     if (err) return next(err);
 
     if (req.body.name) user.profile.name = req.body.name;
     if (req.body.address) user.address = req.body.address;
 
-    user.save(function(err) {
+    user.save(function (err) {
       if (err) return next(err);
       req.flash('success', 'Successfully Edited your profile');
       return res.redirect('/edit-profile');
