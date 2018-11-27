9ebbded4b5492a5b7d46f7c85f06493b422e46a2
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-11-27 13:33:09 +0530

    Adding Remove feature to remove product from cart

--- routes/main.js
+++ routes/main.js
@@ -10,12 +10,12 @@ function paginate(req, res, next) {
 
   Product
     .find()
-    .skip( perPage * page)
-    .limit( perPage )
+    .skip(perPage * page)
+    .limit(perPage)
     .populate('category')
-    .exec(function(err, products) {
+    .exec(function (err, products) {
       if (err) return next(err);
-      Product.count().exec(function(err, count) {
+      Product.count().exec(function (err, count) {
         if (err) return next(err);
         res.render('main/product-main', {
           products: products,
@@ -26,7 +26,7 @@ function paginate(req, res, next) {
 
 }
 
-Product.createMapping(function(err, mapping) {
+Product.createMapping(function (err, mapping) {
   if (err) {
     console.log("error creating mapping");
     // console.log(err);
@@ -39,23 +39,23 @@ Product.createMapping(function(err, mapping) {
 var stream = Product.synchronize();
 var count = 0;
 
-stream.on('data', function() {
+stream.on('data', function () {
   count++;
 });
 
-stream.on('close', function() {
+stream.on('close', function () {
   console.log("Indexed " + count + " documents");
 });
 
-stream.on('error', function(err) {
+stream.on('error', function (err) {
   console.log(err);
 });
 
-router.get('/cart', function(req, res, next) {
+router.get('/cart', function (req, res, next) {
   Cart
     .findOne({ owner: req.user._id })
     .populate('items.item')
-    .exec(function(err, foundCart) {
+    .exec(function (err, foundCart) {
       if (err) return next(err);
       res.render('main/cart', {
         foundCart: foundCart,
@@ -64,8 +64,8 @@ router.get('/cart', function(req, res, next) {
     });
 });
 
-router.post('/product/:product_id', function(req, res, next) {
-  Cart.findOne({ owner: req.user._id }, function(err, cart) {
+router.post('/product/:product_id', function (req, res, next) {
+  Cart.findOne({ owner: req.user._id }, function (err, cart) {
     cart.items.push({
       item: req.body.product_id,
       price: parseFloat(req.body.priceValue),
@@ -74,26 +74,38 @@ router.post('/product/:product_id', function(req, res, next) {
 
     cart.total = (cart.total + parseFloat(req.body.priceValue)).toFixed(2);
 
-    cart.save(function(err) {
+    cart.save(function (err) {
       if (err) return next(err);
       return res.redirect('/cart');
     });
   });
 });
 
+router.post('/remove', function (req, res, next) {
+  Cart.findOne({ owner: req.user._id }, function (err, foundCart) {
+    foundCart.items.pull(String(req.body.item));
 
-router.post('/search', function(req, res, next) {
+    foundCart.total = (foundCart.total - parseFloat(req.body.price)).toFixed(2);
+    foundCart.save(function (err, found) {
+      if (err) return next(err);
+      req.flash('remove', 'Successfully removed');
+      res.redirect('/cart');
+    });
+  });
+});
+
+router.post('/search', function (req, res, next) {
   res.redirect('/search?q=' + req.body.q);
 });
 
-router.get('/search', function(req, res, next) {
+router.get('/search', function (req, res, next) {
   if (req.query.q) {
     Product.search({
-      query_string: { query: req.query.q}
-    }, function(err, results) {
+      query_string: { query: req.query.q }
+    }, function (err, results) {
       results:
       if (err) return next(err);
-      var data = results.hits.hits.map(function(hit) {
+      var data = results.hits.hits.map(function (hit) {
         return hit;
       });
       res.render('main/search-result', {
@@ -104,7 +116,7 @@ router.get('/search', function(req, res, next) {
   }
 });
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 
   if (req.user) {
     paginate(req, res, next);
@@ -114,19 +126,19 @@ router.get('/', function(req, res, next) {
 
 });
 
-router.get('/page/:page', function(req, res, next) {
-  paginate(req,res,next);
+router.get('/page/:page', function (req, res, next) {
+  paginate(req, res, next);
 });
 
-router.get('/about', function(req, res) {
+router.get('/about', function (req, res) {
   res.render('main/about');
 });
 
-router.get('/products/:id', function(req, res, next) {
+router.get('/products/:id', function (req, res, next) {
   Product
     .find({ category: req.params.id })
     .populate('category')
-    .exec(function(err, products) {
+    .exec(function (err, products) {
       if (err) return next(err);
       res.render('main/category', {
         products: products
@@ -135,8 +147,8 @@ router.get('/products/:id', function(req, res, next) {
 });
 
 
-router.get('/product/:id', function(req, res, next) {
-  Product.findById({ _id: req.params.id }, function(err, product) {
+router.get('/product/:id', function (req, res, next) {
+  Product.findById({ _id: req.params.id }, function (err, product) {
     if (err) return next(err);
     res.render('main/product', {
       product: product
